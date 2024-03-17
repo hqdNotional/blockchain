7b58a8e2cbb911ffaf252537b9d69690e29a0f2b
dqdNotional <datquocha@notional.ventures>
2024-03-17 21:14:34 +0700

    Add check_sync script

---
+++ check_sync.sh
@@ -0,0 +1,12 @@
+echo "step 5"
+
+catching_up="true"
+while [[ "$catching_up" != "false" ]]; do
+  sleep 60;
+  catching_up=$(curl --silent --max-time 3 "http://localhost:26657/status" |jq -r .result.sync_info.catching_up)
+  echo "catching_up=$catching_up"
+done
+
+##############
+echo "synched"
+
