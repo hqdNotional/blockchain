63c51fbff86d545c542394c5184503b3f33a44ef
Ha Quoc Dat <datquocha@notional.ventures>
2024-04-15 18:33:11 +0700

    Add rpc script

--- ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Enter service name [bitsong]:" service
-SWARM_NODE=$(docker service ps $(docker service ls | grep $service | grep -E "rpc_$service\\_" | awk '{print $2}') | awk '{print $4}')
-CONTAINER_ID=$(docker service ps $(docker service ls | grep $service | grep -E "rpc_$service\\_" | awk '{print $2}') | awk '{print $1}')
-
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$SWARM_NODE "docker exec -it $CONTAINER_ID /bin/bash"
