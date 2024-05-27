f57d3354179e07608d49e58ab3ab9378effeba4d
Ha Quoc Dat <datquocha@notional.ventures>
2024-05-27 21:29:30 +0700

    uupdate: synch check script

--- check_sync.sh
+++ check_sync.sh
@@ -1,4 +1,24 @@
-echo "Sync checking"
+#!/bin/bash
+
+black='\033[0;30m'
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+gray='\033[0;37m'
+gray2='\033[1;30m'
+red2='\033[1;31m'
+green2='\033[1;32m'
+yellow='\033[1;33m'
+blue2='\033[1;34m'
+purple2='\033[1;35m'
+cyan2='\033[1;36m'
+white='\033[1;37m'
+nc='\033[0m' # No Color
+
+echo -e "${green}step 5:${nc} ${yellow}synchronization checking${nc}"
 
 catching_up="true"
 while [[ "$catching_up" != "false" ]]; do
@@ -8,5 +28,4 @@ while [[ "$catching_up" != "false" ]]; do
 done
 
 ##############
-echo "synched"
-
+echo -e "${cyan}synched${nc}"
