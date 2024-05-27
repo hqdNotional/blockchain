c96b07d45637b0b9a029edab6264daac5f9c8fc6
Ha Quoc Dat <datquocha@notional.ventures>
2024-05-27 21:25:44 +0700

    update: upgrade: goleveldb

--- upgrade_golevel.sh
+++ upgrade_golevel.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+black='\033[0;30m'
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+gray='\033[0;37m'
+gray2='\033[1;30m'
+red2='\033[1;31m'
+green2='\033[1;32m'
+yellow='\033[1;33m'
+blue2='\033[1;34m'
+purple2='\033[1;35m'
+cyan2='\033[1;36m'
+white='\033[1;37m'
+nc='\033[0m' # No Color
+
 # get new version as argument
 new_version = $1
 
@@ -21,6 +39,11 @@ then
   exit
 fi
 
+build_binary () {
+  
+# get function args
+new_version = $1
+
 # get repository name
 repo_name=$(basename $git_repo |cut -d. -f1)
 
@@ -36,14 +59,47 @@ git checkout $new_version
 
 # stop chain
 supervisorctl stop chain
+sleep 5
 
 # build new binary
 make install
+sleep 5
 
 # start chain
 supervisorctl start chain
+sleep 5
 
-echo "step 5: synch checking..."
+}
+
+##################
+# 1. stop chain & delete /var/log/hain.err.log
+echo -e "${green}step 1:${nc} ${yellow}clean old logs${nc}"
+supervisorctl stop chain
+sleep 5;
+
+echo "" > /var/log/chain.err.log
+
+##################
+# 2. build and run new binary
+echo -e "${green}step 2:${nc} ${yellow}build & run old binary [${nc} ${red}$version${nc} ${yellow}]${nc}"
+build_binary $version
+
+##################
+# 3. check for UPGRADE appear in logs
+echo -e "${green}step 3:${nc} ${yellow}wait until see UPGRADE from logs${nc}"
+# wait until see UPGRADE
+tail -f /var/log/chain.err.log |sed '/UPGRADE\(.*\)NEEDED/ q'
+wait
+sleep 5;
+
+##################
+# 4. build and run new binary
+echo -e "${green}step 4:${nc} ${yellow}build & run new binary [${nc} ${blue}$version_new${nc} ${yellow}]${nc}"
+build_binary $new_version
+
+##################
+# 5. check for synch
+echo -e "${green}step 5:${nc} ${yellow}synchronization checking${nc}"
 
 catching_up="true"
 while [[ "$catching_up" != "false" ]]; do
@@ -53,5 +109,4 @@ while [[ "$catching_up" != "false" ]]; do
 done
 
 ##############
-echo "synched"
-
+echo -e "${cyan}synched${nc}"
