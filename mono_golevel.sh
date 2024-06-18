fe3a83a930e36c667246df96f11b7b2ed7ea60c9
Ha Quoc Dat <datquocha@notional.ventures>
2024-06-18 09:39:06 +0700

    U: mono golevel - activate env

--- mono_golevel.sh
+++ mono_golevel.sh
@@ -2,6 +2,24 @@
 
 VERSION=$1
 
+# get new version as argument
+new_version="$1"
+
+# check if env.sh exists
+if [ -f $HOME/env.sh ]
+then
+  source $HOME/env.sh
+else
+  echo "No env.sh found"
+  exit
+fi
+
+# get repository name
+repo_name=$(basename $git_repo |cut -d. -f1)
+
+# change directory to repository name
+cd $repo_name
+
 supervisorctl stop chain
 git reset --hard
 git fetch --all --tags
