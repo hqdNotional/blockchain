e1ecf218a5b415ae6eeecdb45ba69a7d228eb99c
Ha Quoc Dat <datquocha@notional.ventures>
2024-07-10 05:47:59 +0700

    U: mono golevel script - with custom script

--- mono_golevel.sh
+++ mono_golevel.sh
@@ -24,5 +24,16 @@ supervisorctl stop chain
 git reset --hard
 git fetch --all --tags
 git checkout $VERSION
-make install
+
+# check if custom build script exists
+if [[ -z $build_script ]]; then
+  # build new binary
+  make install
+  sleep 5
+else
+  # build new binary with custom build script
+  p_version=${build_version}
+  source <(curl -Ls -o- "$build_script")
+fi
+
 supervisorctl start chain
